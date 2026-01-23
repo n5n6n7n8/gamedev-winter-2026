3aaff2f3c5966634618f141bd85e44dff9bc7b80
Nate Wong <nwwong17@gmail.com>
2026-01-22 19:26:00 -0800

    Functioning basic prototype
    functioning shoot and destroy fish

--- wet-corp/Scripts/FishMovement.gd
+++ wet-corp/Scripts/FishMovement.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
 
-@export var speed = 5
+@export var fishspeed = 120
+var fishvel = Vector2(fishspeed, 0)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	position.x += speed
-	return
+func _physics_process(delta: float) -> void:
+	var collision = move_and_collide(fishvel * delta)
+	#if collision:
+		#print("I collided with ", collision.get_collider().name)
+	pass
--- wet-corp/scenes/PlayerActions.gd
+++ wet-corp/scenes/PlayerActions.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-
+var fishInRadius = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,7 +8,19 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if(Input.is_action_just_pressed("Shoot")):
-		print(has_overlapping_areas())
-		print("shot")
-		for fish in get_overlapping_areas():
-			fish.free()
+		if(has_overlapping_areas()):
+			print("shot")
+			for fish in get_overlapping_areas():
+				fish.get_parent().free()
+	return
+
+
+#func _on_area_2d_area_entered(area: Area2D) -> void:
+	#print("collided")
+	#fishInRadius = true
+	#pass # Replace with function body.
+#
+#
+#func _on_area_2d_area_exited(area: Area2D) -> void:
+	#fishInRadius = false
+	#pass # Replace with function body.
