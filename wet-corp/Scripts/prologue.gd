8b3d9d7f8fa84a28f5385e6fb954bed6d3fea739
Meeecoh <my.coworker.art@gmail.com>
2026-02-13 19:11:13 -0800

    Renamed prologue scenes, added skip timer progress bar

--- wet-corp/Scripts/prologue.gd
+++ wet-corp/Scripts/prologue.gd
@@ -2,12 +2,22 @@ extends Node
 
 var skip_enabled : bool = false;
 @export var next_scene : PackedScene
+@export var skip_timer_length : float = 1.0;
 
 func _ready() -> void:
 	# create timer so user doesn't accidentally skip by clicking too early
-	$Timer.start()
-	$Timer.connect("timeout", enable_skip)
+	var timer = get_tree().create_timer(skip_timer_length)
+	timer.connect("timeout", enable_skip)
+
+	#progressbar tween to show skip progress
+	var tween = get_tree().create_tween()
+	tween.tween_property($TextureProgressBar, "value", 100, skip_timer_length)
+	
+	#play animation
 	$AnimationPlayer.play("animation")
+	await $AnimationPlayer.animation_finished
+	enable_skip() #enable skip if animation finishes before timer
+	
 	
 func enable_skip():
 	print("skip enabled")
