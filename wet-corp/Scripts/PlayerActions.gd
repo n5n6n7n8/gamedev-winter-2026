d83883b6e3d94073d66553fc47fb06cb1a4f678c
MycoG <125831029+Meeecoh@users.noreply.github.com>
2026-03-04 15:04:44 -0800

    Fix bug with armored fish being added to count whenever hit

--- wet-corp/Scripts/PlayerActions.gd
+++ wet-corp/Scripts/PlayerActions.gd
@@ -50,20 +50,22 @@ func _input(e:InputEvent) -> void:
 			for f in get_overlapping_areas():
 				var fi = f.get_parent()
 				var fish = f.get_parent().get_parent()
-				if fish.is_in_group("red_snapper"): # on red snapper shot
-					_add_fish_to_gameinfo("red_snapper")
-				elif fish.is_in_group("armored_fish"):  # on armored shot
-					_add_fish_to_gameinfo("armored_fish")
-				elif fish.is_in_group("pufferfish"):  # on puffer shot
-					_add_fish_to_gameinfo("pufferfish")
-				elif fish.is_in_group("kissy_fish"):  # on kissy shot
-					GameInfo.heal(15)
-					_add_fish_to_gameinfo("kissy_fish")
-				else:
-					print("error finding fish!!!")
-					return
+				
 				fi.take_damage()
 				if(fi.should_die()):
+					
+					if fish.is_in_group("red_snapper"): # on red snapper shot
+						_add_fish_to_gameinfo("red_snapper")
+					elif fish.is_in_group("armored_fish"):  # on armored shot
+						_add_fish_to_gameinfo("armored_fish")
+					elif fish.is_in_group("pufferfish"):  # on puffer shot
+						_add_fish_to_gameinfo("pufferfish")
+					elif fish.is_in_group("kissy_fish"):  # on kissy shot
+						GameInfo.heal(15)
+						_add_fish_to_gameinfo("kissy_fish")
+					else:
+						print("error finding fish!!!")
+						return
 					fish.queue_free()
 	elif e.is_action_pressed("Reload") && bulletCount <= 0:
 		bulletCount += 15
